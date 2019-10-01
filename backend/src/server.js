6bd2914c20a23de638e7a4ee700d28ce078e5050
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2019-10-01 18:39:03 -0400

    Adicionado dependência de desenvolvimento nodemon

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, rest) => {           //req = requisição ,pega os parametros que o usuário enviar | rest = função, devolve uma resposta para a requisição.
-
+app.get('/', (req, rest) => {                             //req = requisição ,pega os parametros que o usuário enviar | rest = função, devolve uma resposta para a requisição.
+    return rest.json({ message: "Hello Word"});           //envia um objeto com a propriedade message com valor Hello Word
 }); //primeiro parametro rota
 
-app.listen(3333); //Porta em que a aplicação que ficará disponível em localhost:3333
+app.listen(3333);                                         //Porta em que a aplicação que ficará disponível em localhost:3333
 
