42cf5f98acefbc15ca0d70be7deb33e31362151c
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2019-10-01 18:47:45 -0400

    Adicionado script dev para rodar o projeto com o comando yarn dev

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require('express'); //microframeworl pra ajudar com as rotas
 
 const app = express();
 
+// GET(buscar), POST(criar), PUT(alterar), DELETE(deletar) metódos mais importantes para uma API Rest
+
 app.get('/', (req, rest) => {                             //req = requisição ,pega os parametros que o usuário enviar | rest = função, devolve uma resposta para a requisição.
-    return rest.json({ message: "Hello Word"});           //envia um objeto com a propriedade message com valor Hello Word
+    return rest.json({ message: "Hello Word2"});           //envia um objeto com a propriedade message com valor Hello Word
 }); //primeiro parametro rota
 
 app.listen(3333);                                         //Porta em que a aplicação que ficará disponível em localhost:3333
