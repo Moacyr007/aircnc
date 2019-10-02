dcadfd377f1746aa7ddf0f0e90ffc572d391bfef
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2019-10-01 22:44:08 -0400

    Adicionada conexão com mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
-const express = require('express'); //microframework pra ajudar com as rotas
-const routes = require('./routes'); //Importando o arquivo com as rotas
+const express = require('express');     //microframework pra ajudar com as rotas
+const mongoose = require('mongoose'); 
 
+const routes = require('./routes');     //Importando o arquivo com as rotas | Se n passar o caminho relativo (./) e deixar só routes vai procurar uma dependência chamada routes 
 const app = express();
 
+mongoose.connect('mongodb+srv://moacyr007:raiodechocolate@aircnc-o36kh.mongodb.net/AirCnCDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 app.use(express.json()); 
+app.use(routes);
 
-
-app.listen(3333);                                         //Porta em que a aplicação que ficará disponível em localhost:3333
+app.listen(3333);   //Porta em que a aplicação que ficará disponível em localhost:3333
 
