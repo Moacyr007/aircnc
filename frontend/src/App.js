e5cb276a982a5a496f2baccae766815bba3c3841
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2019-10-06 18:43:25 -0400

    Criado pastas separadas para as páginas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,19 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
+
 import './App.css';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState(''); //email é iniciado com valor em branco e depois recebe me tempo real o setEmail que é o estado do email
-
-
-  async function handleSubmit(event){
-    event.preventDefault(); //Evita redirecionamento ao clicar em "Entrar"
-
-    const response = await api.post('/sessions', { email })
-    /* o mesmo que :
-    api.post('/sessions', {
-      email: email
-    })
-    */
-
-    const { _id } = response.data;
-    localStorage.setItem('user', _id);
-  }
   return (
     <div className="container">
       <img src={logo} alt="AirCnC Logo "/>
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          
-          <input type="email" 
-                 id="email" 
-                 placeholder="Seu melhor e-mail"
-                 value = {email}
-                 onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-
-        </form>
+        <Routes />
       </div>
-    </div>
-       
+    </div> 
   );
 }
 
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,42 @@
+import React, { useState }from 'react';
+import api from '../../services/api';
+
+export default function Login(){
+    const [email, setEmail] = useState(''); //email é iniciado com valor em branco e depois recebe me tempo real o setEmail que é o estado do email
+
+
+  async function handleSubmit(event){
+    event.preventDefault(); //Evita redirecionamento ao clicar em "Entrar"
+
+    const response = await api.post('/sessions', { email })
+    /* o mesmo que :
+    api.post('/sessions', {
+      email: email
+    })
+    */
+
+    const { _id } = response.data;
+    localStorage.setItem('user', _id);
+  }
+    return (
+        <>
+            <p>
+            Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+            </p>
+
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="email">E-MAIL *</label>
+            
+            <input type="email" 
+                    id="email" 
+                    placeholder="Seu melhor e-mail"
+                    value = {email}
+                    onChange={event => setEmail(event.target.value)}
+            />
+
+            <button className="btn" type="submit">Entrar</button>
+
+            </form>
+        </>
+    )
+}
