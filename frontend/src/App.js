2db012dee1421f21bac6401be8e892ec9342c4c2
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2019-10-06 18:21:14 -0400

    Configurado salvar email no localStorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState(''); //email é iniciado com valor em branco e depois recebe me tempo real o setEmail que é o estado do email
+
+
+  async function handleSubmit(event){
+    event.preventDefault(); //Evita redirecionamento ao clicar em "Entrar"
+
+    const response = await api.post('/sessions', { email })
+    /* o mesmo que :
+    api.post('/sessions', {
+      email: email
+    })
+    */
+
+    const { _id } = response.data;
+    localStorage.setItem('user', _id);
+  }
   return (
     <div className="container">
       <img src={logo} alt="AirCnC Logo "/>
@@ -13,12 +29,14 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           
           <input type="email" 
                  id="email" 
                  placeholder="Seu melhor e-mail"
+                 value = {email}
+                 onChange={event => setEmail(event.target.value)}
           />
 
           <button className="btn" type="submit">Entrar</button>
@@ -26,7 +44,7 @@ function App() {
         </form>
       </div>
     </div>
-    
+       
   );
 }
 
